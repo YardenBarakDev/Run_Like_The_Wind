551ab9dfc7ea474dd4286f768d24860ca67ea7dd
Yarden70 <Yarden70@gmail.com>
2021-03-14 01:13:59 +0200

    added some logic

--- app/src/main/java/com/YBDev/runlikethewind/activities/StartActivity.java
+++ app/src/main/java/com/YBDev/runlikethewind/activities/StartActivity.java
@@ -1,32 +1,39 @@
 package com.YBDev.runlikethewind.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
-import android.widget.ImageView;
-
 import com.YBDev.runlikethewind.R;
-import com.bumptech.glide.Glide;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class StartActivity extends AppCompatActivity {
 
-    private ImageView StartActivity_IMAGE_background;
-
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        findViewsById();
-        setBackgroundImage();
+
+        //bottomNavigationView.setItemIconTintList(null);
+
+        //navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        //NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        setUpNavigation();
+
+        //NavigationUI.setupWithNavController(bottomNavigationView, Navigation.findNavController(this, R.id.nav_host_fragment));
+    }
+    public void setUpNavigation(){
+        bottomNavigationView =findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment =       (NavHostFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,
+                navHostFragment.getNavController());
     }
 
-    private void findViewsById() {
-        StartActivity_IMAGE_background = findViewById(R.id.StartActivity_IMAGE_background);
-    }
-
-    private void setBackgroundImage(){
-        Glide.with(StartActivity.this)
-                .load(R.drawable.start_page_background)
-                .into(StartActivity_IMAGE_background);
-    }
 }
--- app/src/main/java/com/YBDev/runlikethewind/fragments/Run_Fragment.java
+++ app/src/main/java/com/YBDev/runlikethewind/fragments/Run_Fragment.java
@@ -1,9 +1,19 @@
 package com.YBDev.runlikethewind.fragments;
 
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Run_Fragment extends Fragment {
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+
+    }
 }
